c4cb90b91e567cce04e78e9b5d9eb5e4f691aa52
engrakoh <38119011+engrakoh@users.noreply.github.com>
2018-04-18 15:07:23 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+(function(global) {
+
+    var store = global.store;
+    store.addContact(store.ContactInfo('Osas', '08188131948', 'engrakoh@gmail.com'));
+    store.addContact(store.ContactInfo('Golden', '07033798655', 'golden@yahoo.com'));
+    store.addContact(store.ContactInfo('Lucy', '09054786958', 'lucy@hotmail.com'));
+
+}(this));
